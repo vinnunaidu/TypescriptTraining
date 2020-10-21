f58bda61671d4619b29930312c0c521ca94ac75b
Vinayak <vnaidu.dp@gmail.com>
2020-10-21 07:44:46 +0100

    functions in practise

---
+++ Functions/functions.ts
@@ -0,0 +1,12 @@
+function sayHello(name:String): String {
+    return "Hello " + name;
+}
+
+console.log(sayHello("Ben"));
+
+function voidFunction() : void {
+    console.log("I don't return anything");
+}
+
+voidFunction();
+
